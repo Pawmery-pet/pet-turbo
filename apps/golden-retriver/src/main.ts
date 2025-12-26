aaab30c85cabf5fead6f6118f1bd28857cf592ff
norriswu0 <norris.wu.au+github@outlook.com>
2025-12-26 00:10:34 +0000

    format files

--- apps/golden-retriver/src/main.ts
+++ apps/golden-retriver/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import {
-  FastifyAdapter,
+	FastifyAdapter,
 	type NestFastifyApplication,
 } from "@nestjs/platform-fastify";
 import { AppModule } from "./app.module";
